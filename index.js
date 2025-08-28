f07f51b14baac9db4295865acd91889efefe4864
Meche221 <binaymeche22@gmail.com>
2025-08-28 21:35:58 +0545

    shitty bug

---
+++ index.js
@@ -0,0 +1,10 @@
+// Swiper
+var swiper = new Swiper(".swiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
